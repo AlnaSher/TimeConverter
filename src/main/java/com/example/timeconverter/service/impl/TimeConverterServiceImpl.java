3cd9c606d9c4313b3b5f1149fbebe95e7e519061
AlnaSher <shershnevahelen@gmail.com>
2024-02-27 01:29:50 +0300

    use url

--- src/main/java/com/example/timeconverter/service/impl/TimeConverterServiceImpl.java
+++ src/main/java/com/example/timeconverter/service/impl/TimeConverterServiceImpl.java
@@ -4,13 +4,10 @@ import com.example.timeconverter.model.TimeMS;
 import com.example.timeconverter.repository.TimeMSDAO;
 import com.example.timeconverter.service.TimeConverterService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -19,20 +16,11 @@ public class TimeConverterServiceImpl implements TimeConverterService {
     private final TimeMSDAO repository;
     @Override
     public List<TimeMS> getCurrentTime() {
-        ZoneId currentTimeZone = ZoneId.systemDefault();
-        String timezoneString = currentTimeZone.getId();
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://worldtimeapi.org/api/ip";
 
-        ZoneOffset gmtOffset = OffsetDateTime.now().getOffset();
-        String gmtOffsetString = gmtOffset.toString();
-
-        ZonedDateTime currentTime = ZonedDateTime.now();
-        ZonedDateTime gmtTime = currentTime.withZoneSameInstant(ZoneId.ofOffset("GMT", gmtOffset));
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String gmtTimeString = gmtTime.format(formatter);
-
-        TimeMS timeMS = new TimeMS(gmtTimeString, gmtOffsetString, timezoneString);
-
-        repository.saveTime(timeMS);
+        ResponseEntity<TimeMS> response = restTemplate.getForEntity(url, TimeMS.class);
+        repository.saveTime(response.getBody());
         return repository.getCurrentTime();
     }
 
