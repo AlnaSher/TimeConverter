02321b2fc1d9b9ebd48b1ee108d8391623a2eca8
AlnaSher <shershnevahelen@gmail.com>
2024-02-26 13:08:29 +0300

    change param

--- src/main/java/com/example/timeconverter/model/TimeMS.java
+++ src/main/java/com/example/timeconverter/model/TimeMS.java
@@ -6,20 +6,7 @@ import lombok.*;
 @Builder
 public class TimeMS {
     private String abbreviation;
-    private String clientIP;
-    private String dateTime;
-    private int dayWeak;
-    private int dayYear;
-    private boolean dst;
-    private String dstFrom;
-    private int dstOffset;
-    private String dstUntil;
-    private int rawOffset;
+    private String datetime;
     private String timezone;
-    private int unixTime;
-    private String utsDateTime;
-    private String utsOffset;
-    private int weekNumber;
-
-
 }
+
--- src/main/java/com/example/timeconverter/service/impl/TimeConverterServiceImpl.java
+++ src/main/java/com/example/timeconverter/service/impl/TimeConverterServiceImpl.java
@@ -4,7 +4,9 @@ import com.example.timeconverter.model.TimeMS;
 import com.example.timeconverter.repository.TimeMSDAO;
 import com.example.timeconverter.service.TimeConverterService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 @Service
@@ -13,6 +15,11 @@ public class TimeConverterServiceImpl implements TimeConverterService {
     private final TimeMSDAO repository;
     @Override
     public List<TimeMS> getCurrentTime() {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://worldtimeapi.org/api/ip";
+
+        ResponseEntity<TimeMS> response = restTemplate.getForEntity(url, TimeMS.class);
+        repository.saveTime(response.getBody());
         return repository.getCurrentTime();
     }
 
